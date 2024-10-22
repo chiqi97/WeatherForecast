2b4fd968f0f028642f4bfaeb576992c2dfb6cf65
CezaryZysk <cezaryzysk@noaignite.com>
2024-10-22 23:35:52 +0200

    Add migration

---
+++ WeatherForecast.Data/Migrations/20241022212743_Initial.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherForecast.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coordinates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Longitude = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Latitude = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coordinates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeatherForecasts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Temperature = table.Column<double>(type: "REAL", nullable: false),
+                    Interval = table.Column<double>(type: "REAL", nullable: false),
+                    Time = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CoordinateId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherForecasts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WeatherForecasts_Coordinates_CoordinateId",
+                        column: x => x.CoordinateId,
+                        principalTable: "Coordinates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeatherForecasts_CoordinateId",
+                table: "WeatherForecasts",
+                column: "CoordinateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherForecasts");
+
+            migrationBuilder.DropTable(
+                name: "Coordinates");
+        }
+    }
+}
